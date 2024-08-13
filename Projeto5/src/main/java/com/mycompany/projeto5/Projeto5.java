a0b0814e55199ccc2d398f2da3b269bace73c20f
Bruno de Paula <brunohpaula1995@gmail.com>
2024-08-12 23:30:46 -0300

    Projeto Banco

---
+++ Projeto5/src/main/java/com/mycompany/projeto5/Projeto5.java
@@ -0,0 +1,27 @@
+package com.mycompany.projeto5;
+
+public class Projeto5 {
+
+    public static void main(String[] args) {
+        ContaBanco p1 = new ContaBanco();
+        p1.setNumConta(1111);
+        p1.setDono("Jubileu");
+        p1.abrirConta("CC");
+        
+        
+        ContaBanco p2 = new ContaBanco();
+        p2.setNumConta(2222);
+        p2.setDono("Creuza");
+        p2.abrirConta("CP");
+        
+        p1.depositar(100);
+        p2.depositar(500);
+        p2.sacar(100);
+        
+        
+        p1.estadoAtual(); 
+        p2.estadoAtual();
+        
+        
+    }
+}

17321cd3de7484f978a1d8b42bf12350466d9c59
Bruno de Paula <brunohpaula1995@gmail.com>
2024-08-12 22:26:08 -0300

    Getters e Setters

---
+++ Projeto4/src/main/java/com/mycompany/projeto4/Projeto4.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.projeto4;
+
+public class Projeto4 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("Nic",  0.4f, "Amarelo");
+    
+    }
+}

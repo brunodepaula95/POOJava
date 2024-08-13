4bec857f3081e89ef22199b6d2c9d7044a6d06e6
Bruno de Paula <brunohpaula1995@gmail.com>
2024-08-12 21:45:37 -0300

    atualizando

---
+++ Projeto3 - Copia/src/main/java/com/mycompany/projeto3/Projeto3.java
@@ -0,0 +1,15 @@
+package com.mycompany.projeto3;
+
+public class Projeto3 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.modelo = "Bis Cristal";
+        c1.cor = "Azul";
+        c1.carga = 80;
+        //c1.tampada = true;
+        c1.status();
+        
+        
+    }
+}

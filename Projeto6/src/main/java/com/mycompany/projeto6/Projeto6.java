ede390d93a3ed4a7c9ab163dc800d8e752fb549f
Bruno de Paula <brunohpaula1995@gmail.com>
2024-08-14 15:42:07 -0300

    Encapsulamento

---
+++ Projeto6/src/main/java/com/mycompany/projeto6/Projeto6.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.projeto6;
+
+public class Projeto6 {
+
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
+    }
+}

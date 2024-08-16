73ef5aec94d8afa74c6a79a2ca313fff38dca556
Bruno de Paula <brunohpaula1995@gmail.com>
2024-08-15 22:07:28 -0300

    Polimorfismo com Sobrecarga

---
+++ Projeto13/src/main/java/com/mycompany/projeto13/Lobo.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.projeto13;
+
+public class Lobo extends Mamifero {
+    
+    @Override
+    public void emitirSom(){
+        System.out.println("Auuuuuu!");
+    }
+}
---
+++ Projeto13/src/main/java/com/mycompany/projeto13/Projeto13.java
@@ -0,0 +1,12 @@
+package com.mycompany.projeto13;
+
+
+public class Projeto13 {
+
+    public static void main(String[] args) {
+        
+        Cachorro c = new Cachorro();
+        
+        c.reagir(true);
+    }
+}
